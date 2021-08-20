b793e950fc6bdab841f20c69b376a770767c4bbf
KotoJeep <omaigader@gmail.co>
2021-08-20 16:58:22 +0700

    feat: added task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	// your solution
+	let stack = [],
+		configObj = Object.fromEntries(bracketsConfig.map((arr) => arr.reverse()));
+
+	for (let i = 0; i < str.length; i++) {
+		const bracket = str[i];
+		// проверяем, является ли скобка закрывающей и есть ли в стеке соответствующая ей открывающая
+		if (Object.keys(configObj).includes(bracket) && stack[stack.length - 1] === configObj[bracket]) {
+			stack.pop();
+		} else {
+			stack.push(bracket);
+		}
+	}
+	return stack.length === 0;
+};
